d98bcfa87c6486eb67908006aa760036e022f275
Its_Racoon <154086150+ItsRacoon@users.noreply.github.com>
2024-02-18 23:49:21 +0530

    Update dv2.py

--- dv2.py
+++ dv2.py
@@ -1,18 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-dt=pd.read_csv("melasales.csv")
-#1----------------------------------
-
-dt.plot(kind='line',color=['red','blue','brown'])
-plt.title("Mela sales")
+import pandas as pd 
+import matplotlib.pyplot as plt 
+df=pd.read_csv("melasales.csv")
+df.plot(kind='line',color=['red','blue', 'brown'])
+plt.title("MELA SALES REPOT")
 plt.xlabel("days")
-plt.ylabel("sales in rs")
+plt.ylabel("sales in Rs")
 plt.show()
 
-#2----------------------------------
+//  2
 
-dt.plot(kind='line',x='days',color=['red','blue','brown'])
-plt.title("Mela sales")
+
+import pandas as pd 
+import matplotlib.pyplot as plt 
+df=pd.read_csv("melasales.csv")
+df.plot(kind='bar',color=['red','blue', 'brown'])
+plt.title("MELA SALES REPOT")
 plt.xlabel("days")
-plt.ylabel("sales in rs")
+plt.ylabel("sales in Rs")
 plt.show()
