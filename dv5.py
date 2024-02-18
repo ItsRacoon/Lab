03c787f0eafe7d4d2bcedb83742bea4a132e18d0
Its_Racoon <154086150+ItsRacoon@users.noreply.github.com>
2024-02-18 23:51:28 +0530

    Update dv5.py

--- dv5.py
+++ dv5.py
@@ -1,22 +1,23 @@
+import numpy as np 
 import matplotlib.pyplot as plt
-import numpy as np
-
-discount = [10, 20, 30, 40, 50]
-sale_in_rupees = [40000, 45000, 48000, 50000, 100000]  # Corrected the missing value in sale_in_rupees
-
-# 1-------------------------------
-plt.scatter(discount, sale_in_rupees)
-plt.xlabel("Discount")
-plt.ylabel("Sales")
+sales_in_Rs = np.array([40000, 45000, 48000, 50000, 100000])
+discount=np.array([10, 20, 30, 40, 50])
+plt.scatter(x=discount, y=sales_in_Rs)
+plt.title("DISCOUNT VS SALES")
+plt.xlabel=("Discount")
+plt.ylabel=("Sales")
 plt.show()
 
-# 2--------------------------------
-dis = np.array(discount)
-sales = np.array(sale_in_rupees)
-size = dis * 10  # Adjusting marker size based on discount
 
-plt.scatter(x=dis, y=sales, s=size)
-plt.xlabel("Discount")
-plt.ylabel("Sales")
-plt.title("Scatter Plot with Size Adjustment")
+
+
+import numpy as np 
+import matplotlib.pyplot as plt
+sales_in_Rs = np.array([40000, 45000, 48000, 50000, 100000])
+discount=np.array([10, 20, 30, 40, 50])
+size=discount*10
+plt.scatter(x=discount, y=sales_in_Rs, s=size, color='red', linewidths=3, marker='*', edgecolor='blue')
+plt.title("DISCOUNT VS SALES")
+plt.xlabel=("Discount")
+plt.ylabel=("Sales")
 plt.show()
