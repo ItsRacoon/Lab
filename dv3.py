bc93d2646b59eb85ff943960e3048c91f4295683
Its_Racoon <154086150+ItsRacoon@users.noreply.github.com>
2024-02-18 23:50:14 +0530

    Update dv3.py

--- dv3.py
+++ dv3.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data=pd.read_csv("Min_max.csv",usecols=['ANNUAL - MIN','ANNUAL - MAX'])
+data = pd.read_csv("min_max.csv",usecols=["ANNUAL_MAX", "ANNUAL_MIN"])
 df=pd.DataFrame(data)
-#1------------------------------
-df.plot(kind='hist',y='ANNUAL - MIN',title='Annual minimum temperature(1901-2017)')
-plt.xlabel("temp")
-plt.ylabel("number of times")
+df.plot(kind='hist', y="ANNUAL_MAX", title="ANNUAL MAXIMUM TEMPERATURE", color=['blue', 'orange'])
+plt.xlabel=("Temperature")
+plt.ylabel=("Number of times")
+plt.show()
 
-#2---------------------------------
-df.plot(kind='hist',title='Annual minimum temperature(1901-2017)')
-plt.xlabel("temp")
-plt.ylabel("number of times")
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("min_max.csv",usecols=["ANNUAL_MAX"])
+df=pd.DataFrame(data)
+df.plot(kind='hist', y="ANNUAL_MAX", title="ANNUAL MAXIMUM TEMPERATURE", color=['blue'])
+plt.xlabel=("Temperature")
+plt.ylabel=("Number of times")
 plt.show()
